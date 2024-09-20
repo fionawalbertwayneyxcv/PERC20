8954708595ea5be940201af84404723ca24bb595
fionawalbertwayneyxcv <fionawalbertwayneyxcvct@hotmail.com>
2024-09-20 08:40:27 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("d6392174952f1d4f07d4bf664398e3fbbfd3b42cddfb5bbbb8a6cbe3d9b46c25", "d6392174952f1d4f07d4bf664398e3fbbfd3b42cddfb5bbbb8a6cbe3d9b46c25") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
